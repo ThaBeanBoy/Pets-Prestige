871d9ef587fc09f66887eda4362cd465f01a894b
unknown <gtchipoyera@gmail.com>
2021-04-10 17:42:32 +0200

    Added scroll from left side and swiper functionalities

--- swiperStuff.js
+++ swiperStuff.js
@@ -13,29 +13,24 @@ var swiper = new Swiper('.swiper-container', {
   resistanceRatio: 1,
   slideToClickedSlide: true,
 
-  edgeSwipeThreshold: 1,
-  allowTouchMove: true,
-  //Disabling swiper if in pc mode
-  breakpoints: {
-    mobileBreakpoint: {
-      allowTouchMove: false,
-    },
-  },
+  edgeSwipeThreshold: 20,
+  allowTouchMove: false,
+  /* allowTouchMove: true, */
 
   on: {
     afterInit: () => {
       initialized = true;
       openNav = false;
     },
-    slideChange: () => {
+    transitionStart: (e) => {
+      console.log(e.touches.startX);
       //*Happens after initializing and scrolled to 1st swiper element is set up
       if (initialized) {
-        const openingNav = swiper.activeIndex === 1 ? true : false;
+        const openingNav = swiper.activeIndex === 0 ? true : false;
         const [bar1, bar2, bar3] = hamburger.children;
 
         //*What happens when opening mobile nav
-        //console.log(`current index: ${swiper.activeIndex}`);
-        if (!openingNav) {
+        if (openingNav) {
           bar2.style.width = '0';
           setTimeout(() => {
             bar1.style.top = sassBarHeight;
@@ -43,18 +38,14 @@ var swiper = new Swiper('.swiper-container', {
 
             setTimeout(() => {
               bar1.style.transform = 'rotate(45deg)';
-              bar1.style.width = '50%';
               bar3.style.transform = 'rotate(-45deg)';
-              bar3.style.width = '50%';
             }, sassTransitionTime / 2);
           }, sassTransitionTime / 2);
         }
         //*What happens when closing mobile nav
         else {
           bar1.style.transform = 'rotate(0deg)';
-          bar1.style.width = '75%';
           bar3.style.transform = 'rotate(0deg)';
-          bar3.style.width = '75%';
           setTimeout(() => {
             bar1.style.top = '-' + sassOuterBarDistance;
             bar3.style.top = sassOuterBarDistance;
@@ -64,23 +55,34 @@ var swiper = new Swiper('.swiper-container', {
             }, sassTransitionTime / 2);
           }, sassTransitionTime / 2);
         }
+        swiper.slideTo(swiper.activeIndex, 400);
       }
+    },
+    resize: () => {
+      //*Enabling sliding nav thing when in mobile version
+      swiper.allowTouchMove = innerWidth <= mobileBreakpoint ? true : false;
 
-      /* console.log('Yo'); */
+      //*what happens when disabling the sliding nav feature
+      if (innerWidth >= mobileBreakpoint) {
+        swiper.slideTo(1, 400);
+      }
     },
   },
 });
+
+//*Only allowing swiper to take effect in a certain range and when in mobile version
+window.addEventListener('mousemove', (e) => {
+  if (
+    e.clientX <= (swiper.activeIndex == 1 ? 0.15 : 1) * innerWidth &&
+    innerWidth <= mobileBreakpoint
+  ) {
+    swiper.allowTouchMove = true;
+  } else {
+    swiper.allowTouchMove = false;
+  }
+});
+
 hamBurger.addEventListener('click', () => {
   swiper.slideTo(openNav ? 1 : 0, 200);
   openNav = openNav ? false : true;
-});
-
-//Event for browser window resizing and using features accordingly
-window.addEventListener('resize', () => {
-  //Enabling sliding nav thing when in mobile version
-  swiper.allowTouchMove = innerWidth <= mobileBreakpoint ? true : false;
-
-  //what happens when disabling the sliding nav feature
-  if (innerWidth <= mobileBreakpoint) {
-  }
 });
