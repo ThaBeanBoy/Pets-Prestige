1133eaacbd77c2f41a83d3185844797bdfe69b78
unknown <gtchipoyera@gmail.com>
2021-08-08 12:23:13 +0200

    Got the basics of Sortable js, caret and nest should be merged, because sortable treats the head and caret as seperate entities

--- jsFiles/memStuff/displayChillun.js
+++ jsFiles/memStuff/displayChillun.js
@@ -1,50 +1,40 @@
-const closeFlowBox = () => {
-    //Removing save-changes button
-    $('.save-changes').remove();
+
   
-    //Closing edit flow box
-    $('.edit-flow-box').css('width', '0');
-  
-    //Emptying flow body
-    $('.edit-flow-body').empty();
-  };
-  
-  //Used to create .nested elements
-  const displayChillun = (ul, kinders) => {
-    //Setting up new ul element
-    let newUl = $(`<ul class="nested"></ul>`);
-  
-    kinders.forEach((n) => {
-      //Setting up the done class
-      const done = n.state ? 'done' : 'not-done';
-  
-      //If n is a caret
-      if (n.isCaret()) {
-        //Setting up the data-state
-        const show = n.showing ? 'showing' : 'not-showing';
-  
-        //Craeting the div
-        const div = $(
-          `<div class="caret ${done}" data-state="${show}"><span class="task-txt">${n.title}</span><span class="icon-ellipsis-vert optBtns"></span></div>`
-        );
-  
-        //Appending the div to the newUl
-        newUl.append(div);
-  
-        //Recursive function
-        displayChillun(newUl, n.children);
-      }
-      //If n is just a task
-      else {
-        //Craeting the li
-        const li = $(
-          `<li class="${done}"><span class="task-txt">${n.title}</span><span class="icon-ellipsis-vert optBtns"></span></li>`
-        );
-  
-        //Appending the new li to the newUl
-        newUl.append(li);
-      }
-    });
-    $(ul).append(newUl);
-  };
-  
+//Used to create .nested elements
+const displayChillun = (ul, kinders) => {
+  //Setting up new ul element
+  let newUl = $(`<ul class="nested"></ul>`);
+
+  kinders.forEach((n) => {
+    //Setting up the done class
+    const done = n.state ? 'done' : 'not-done';
+
+    //If n is a caret
+    if (n.isCaret()) {
+      //Setting up the data-state
+      const show = n.showing ? 'showing' : 'not-showing';
+
+      //Craeting the div
+      const div = $(
+        `<li class="caret ${done}" data-state="${show}"><span class="task-txt">${n.title}</span><span class="icon-ellipsis-vert optBtns"></span></li>`
+      );
+
+      //Appending the div to the newUl
+      newUl.append(div);
+
+      //Recursive function
+      displayChillun(newUl, n.children);
+    }
+    //If n is just a task
+    else {
+      //Craeting the li
+      const li = $(
+        `<li class="${done}"><span class="task-txt">${n.title}</span><span class="icon-ellipsis-vert optBtns"></span></li>`
+      );
+
+      //Appending the new li to the newUl
+      newUl.append(li);
+    }
+  });
+  $(ul).append(newUl);
+};
---
+++ jsFiles/sortableStuff.js
@@ -0,0 +1,22 @@
+const allFather = document.querySelector('.allFather');
+
+//* Sortabilisng all nested elements
+let allNested = document.querySelectorAll('.nested');
+for(let i=0; i<allNested.length; i++){
+    new Sortable(allNested[i], {
+        group: 'nested',
+		animation: 150,
+		fallbackOnBody: true,
+		swapThreshold: 0.65
+    })
+};
+
+//* Initiating sortable
+let sorty = new Sortable(allFather, {
+    group: 'nested',
+    handle: '.optBtns',
+
+    onEnd: (evt)=>{
+        console.log(evt)
+    }
+})
